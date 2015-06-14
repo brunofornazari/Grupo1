b5359d371a5b284a3e940fd9254103d39359541b
brunofornazari <bruno_fornazari@hotmail.com>
2015-06-14 19:52:33 -0300

    OpenCv working
    Opencv funcionando

--- app/src/main/java/com/example/dell/grupo1/PictureActivity.java
+++ app/src/main/java/com/example/dell/grupo1/PictureActivity.java
@@ -5,22 +5,52 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 import org.opencv.imgproc.*;
 
 
 public class PictureActivity extends ActionBarActivity {
 
     static Bitmap acumulator;
-    ImageView Pic;
-
+    static ImageView Pic;
+    private static final String TAG = "MyActivity";
+    static {
+        if (!OpenCVLoader.initDebug())
+            Log.d("ERROR", "Unable to load OpenCV");
+        else
+            Log.d("SUCCESS", "OpenCV loaded");
+    }
     public void Snap(View v) {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(i, 1);
     }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    System.loadLibrary("ScannerApp");
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
 
 
 
@@ -45,6 +75,7 @@ public class PictureActivity extends ActionBarActivity {
         setContentView(R.layout.activity_picture);
         Pic = (ImageView)findViewById(R.id.Picture);
         Snap(new View(this));
+        Mat teste = new Mat();
     }
 
     @Override
